71b4b1d800d2e730b731fda83b54a0571d1ced36
vasantha796 <vasanthapathlavath2121@gmail.com>
2025-08-19 15:52:30 +0530

    adding

--- armstrong.py
+++ armstrong.py
@@ -11,26 +11,26 @@ if s==sum:
     print("Armstrong")
 else:
     print("nt an Armstrong") 
+153
+
+def Armstrong(n):
+    s = n
+    b = len(str(n))
+    total = 0
+    
+    while n != 0:
+        r = n % 10
+        total = total + (r ** b)
+        n = n // 10
+    
+    if s == total:
+        print("Armstrong number")
+    else:
+        print("is not an Armstrong number")
+
+n = int(input("Enter your number: "))
+Armstrong(n)
 # 153
-
-# def Armstrong(n):
-#     s = n
-#     b = len(str(n))
-#     total = 0
-    
-#     while n != 0:
-#         r = n % 10
-#         total = total + (r ** b)
-#         n = n // 10
-    
-#     if s == total:
-#         print("Armstrong number")
-#     else:
-#         print("is not an Armstrong number")
-
-# n = int(input("Enter your number: "))
-# Armstrong(n)
-# # 153
        
  
 
