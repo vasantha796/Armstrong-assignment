a06ec50d57998aa474c6cfeac78f333d9ce3a775
vasantha796 <vasanthapathlavath2121@gmail.com>
2025-08-20 20:15:59 +0530

    armstrongprime.py

--- armstrongprime.py
+++ armstrongprime.py
@@ -8,22 +8,22 @@
 #     if is_armstrong(i):
 #         print(i)    
 
-# def is_armstrong(lower,upper):
-#     for n in range(lower,upper+1):
-#         s=n
-#         count=0
-#         while s>0:
-#             count+=1
-#             s//=10
-#         s=n
-#         total=0
-#         while s>0:
-#             digit=s%10
-#             total+=digit**count
-#             s//=10
-#         if n==total:
-#             print(n)
-# is_armstrong(100,2000)                        
+def is_armstrong(lower,upper):
+    for n in range(lower,upper+1):
+        s=n
+        count=0
+        while s>0:
+            count+=1
+            s//=10
+        s=n
+        total=0
+        while s>0:
+            digit=s%10
+            total+=digit**count
+            s//=10
+        if n==total:
+            print(n)
+is_armstrong(100,2000)                        
  # 153
 # 370
 # 371
@@ -31,15 +31,16 @@
 # 1634
 
 # armstrong number is prime 
-flag=True
-def is_prime(lower,upper):
+
+def is_prime(n):
+    if n<2:
+        return False
     for i in range(2,int(n**0.5)+1):
-        if n%i==0:
-            flag=False
-            break
-    if flag==True:
-        print(n)
+        return False
+    return True
+
 lower=int(input("enter lower range:"))
-upper=int(input("enter upper range:")) 
-is_prime(lower,upper)       
-        
+upper=int(input("enter upper range:"))
+for num in range(lower,upper+1):
+    if is_armstrong(n) and is_prime(n):
+        print(n)
